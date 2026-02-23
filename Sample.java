64171b4767525aebfe3b8a20cafa6a9a0d612c8d
takudzwa <takchiks@gmail.com>
2026-02-22 20:42:12 -0500

    [removeDuplicates, merge 2 sorted arrays, searchMatrix]

--- Sample.java
+++ Sample.java
@@ -5,3 +5,71 @@
 // Three line explanation of solution in plain english
 
 // Your code here along with comments explaining your approach
+
+public class Sample {
+
+    // Time Complexity : O(n)
+    // Space Complexity : O(1)
+    // Did this code successfully run on Leetcode : yes
+    public int removeDuplicates(int[] nums) {
+        int i = 0;
+        int k = 0;
+        int count = 1;
+        int prev = 10001;
+        while ( i < nums.length) {
+            if (prev != nums[i]) {
+                count = 1;
+            }
+            if (count <= 2) {
+                nums[k] = nums[i];
+                k++;
+                count++;
+            }
+            prev = nums[i];
+            i++;
+        }
+        return k;
+    }
+
+    // Time Complexity : O(m + n)
+    // Space Complexity : O(1) no extra space
+    // Did this code successfully run on Leetcode : yes
+
+    // start inserting from m - 1 index
+    // if m becomes 0 first; then just add the remaining elements from nums2 to nums1
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int l = nums1.length - 1;
+        m--;
+        n--;
+        while (m >= 0 && n >= 0) {
+            nums1[l--] = nums1[m] < nums2[n] ? nums2[n--] : nums1[m--];
+        }
+        while (n >= 0) {
+            nums1[l--] = nums2[n--];
+        }
+    }
+
+    // Time Complexity : O(m + n)
+    // Space Complexity : O(1)
+    // Did this code successfully run on Leetcode : yes
+
+    // start searching from bottom left corner of matrix
+    // if curr element is greater than target search upwards
+    // else search rightwards
+    // return true if found
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i = matrix.length - 1;
+        int j = 0;
+        while (i >=0 && j < matrix[0].length) {
+            if (matrix[i][j] == target) {
+                return true;
+            }
+            if (matrix[i][j] > target) {
+                i--;
+            } else {
+                j++;
+            }
+        }
+        return false;
+    }
+}
